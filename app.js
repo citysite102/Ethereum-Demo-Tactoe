300c06be100403c0a5a0c266ca23566fca6b1c88
Samuel <citysite1025@gmail.com>
2018-04-16 16:39:47 +0800

    Update js file

--- app.js
+++ app.js
@@ -38504,8 +38504,8 @@ window.App = {
     account = accounts[1];
   },
   createNewGame: function() {
-    // TicTacToe.new(100, {from:account, value:web3.toWei(0.1,"ether"), gas:3000000}).then(instance => {
-      TicTacToe.new(100).then(instance => {
+    TicTacToe.new(100, {from:account, value:web3.toWei(0.1,"ether"), gas:3000000}).then(instance => {
+      // TicTacToe.new(100).then(instance => {
       ticTacToeInstance = instance;
 
       console.log(instance);
